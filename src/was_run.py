d262ba96ef54e3186f1193eeda14e08941020dea
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-17 20:58:15 +0000

    move `wasRun` flag to `setUp()` at `WasRun` class
    Checklist:
    - Invoke setUp first 👈🏼
    - Invoke tearDown afterward
    - Invoke tearDown even if the test method fails
    - Run multiple tests
    - Report collected results
    
    ---
    
    Output:
    ➜ python3 src/TestCaseTest.py
    Traceback (most recent call last):
      File "tdd-xunit-example/src/TestCaseTest.py", line 18, in <module>
        TestCaseTest("testRunning").run()
      File "tdd-xunit-example/src/TestCase.py", line 12, in run
        method()
      File "tdd-xunit-example/src/TestCaseTest.py", line 8, in testRunning
        assert (not test.wasRun)
                    ^^^^^^^^^^^
    AttributeError: 'WasRun' object has no attribute 'wasRun'

--- src/was_run.py
+++ src/was_run.py
@@ -3,10 +3,10 @@ from test_case import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def setUp(self) -> None:
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self) -> None:
