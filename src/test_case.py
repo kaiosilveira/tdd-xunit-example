8d6a92fbd9c8abfe92cbcbbb3f4efbd29bb92850
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-17 19:31:22 +0000

    implement `setUp()` at `TestCase`
    Checklist:
    - Invoke setUp first 👈🏼
    - Invoke tearDown afterward
    - Invoke tearDown even if the test method fails
    - Run multiple tests
    - Report collected results

--- src/test_case.py
+++ src/test_case.py
@@ -3,6 +3,10 @@ class TestCase:
     def __init__(self, name) -> None:
         self.name = name
 
+    def setUp(self) -> None:
+        pass
+
     def run(self) -> None:
+        self.setUp()
         method = getattr(self, self.name)
         method()
