ce648595660158151b158f163024e22a85ff6f34
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-17 19:28:29 +0000

    implement `setUp()` at `WasRun`
    Checklist:
    - Invoke setUp first 👈🏼
    - Invoke tearDown afterward
    - Invoke tearDown even if the test method fails
    - Run multiple tests
    - Report collected results
    
    ---
    
    Output:
    ➜ python3 src/TestCaseTest.py
    Traceback (most recent call last):
      File "tdd-xunit-example/src/TestCaseTest.py", line 19, in <module>
        TestCaseTest("testSetup").run()
      File "tdd-xunit-example/src/TestCase.py", line 8, in run
        method()
      File "tdd-xunit-example/src/TestCaseTest.py", line 15, in testSetup
        assert (test.wasSetUp)
                ^^^^^^^^^^^^^
    AttributeError: 'WasRun' object has no attribute 'wasSetUp'

--- src/was_run.py
+++ src/was_run.py
@@ -6,5 +6,8 @@ class WasRun(TestCase):
         self.wasRun = None
         TestCase.__init__(self, name)
 
+    def setUp(self) -> None:
+        self.wasSetUp = 1
+
     def testMethod(self) -> None:
         self.wasRun = 1
