e744108a6c015959aae6588a7beef0b664b20935
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-18 13:56:33 +0000

    return an instance of `TestResult` at `TestCase.run()`
    Checklist:
    - Invoke tearDown even if the test method fails
    - Run multiple tests
    - Report collected results 👈🏼

--- src/test_case.py
+++ src/test_case.py
@@ -1,3 +1,6 @@
+from test_result import TestResult
+
+
 class TestCase:
 
     def __init__(self, name) -> None:
@@ -11,6 +14,7 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
 
     def tearDown(self) -> None:
         pass
