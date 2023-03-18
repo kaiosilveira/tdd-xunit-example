e3e483a58b4970465d55b4dec84bad759a51b7bf
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-18 19:21:38 +0000

    implement `run()` for `TestSuite`
    Checklist:
    - Invoke tearDown even if the test method fails
    - Run multiple tests 👈🏼
    
    ---
    
    Output:
    ➜ python3 src/test_case_test.py
    1 run, 0 failed
    1 run, 0 failed
    1 run, 0 failed
    1 run, 0 failed
    1 run, 1 failed

--- src/test_suite.py
+++ src/test_suite.py
@@ -1,6 +1,15 @@
+from test_result import TestResult
+
+
 class TestSuite:
     def __init__(self) -> None:
         self.tests = []
 
     def add(self, test) -> None:
         self.tests.append(test)
+
+    def run(self) -> None:
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+        return result
