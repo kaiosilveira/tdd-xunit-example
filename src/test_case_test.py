1d44b045c15da7f7b630ce2daa828e9bf7681dc0
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-18 12:42:18 +0000

    remove `setUp()` from `TestCaseTest`
    We are now using it only to configure WasRun, which is in use only in one place, so it's better to just get rid of this extra boilerplate.
    
    Checklist:
    - Invoke tearDown afterward 🎯
    - Invoke tearDown even if the test method fails
    - Run multiple tests
    - Report collected results
    - Log string in WasRun ✅

--- src/test_case_test.py
+++ src/test_case_test.py
@@ -3,12 +3,10 @@ from was_run import WasRun
 
 
 class TestCaseTest(TestCase):
-    def setUp(self) -> None:
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self) -> None:
-        self.test.run()
-        assert ("setUp testMethod " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod " == test.log)
 
 
 TestCaseTest("testTemplateMethod").run()
