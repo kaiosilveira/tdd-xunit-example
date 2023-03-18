751d37d735c67f1c9f4bf17bd6b995ae3ab74ccd
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-18 14:05:42 +0000

    add test for a failed test report
    Checklist:
    - Invoke tearDown even if the test method fails
    - Run multiple tests
    - Report collected results 👈🏼
    
    ---
    
    Output:
    ➜ python3 src/test_case_test.py
    Traceback (most recent call last):
      File "tdd-xunit-example/src/test_case_test.py", line 18, in <module>
        TestCaseTest("testFailedResult").run()
      File "tdd-xunit-example/src/test_case.py", line 17, in run
        method()
      File "tdd-xunit-example/src/test_case_test.py", line 13, in testFailedResult
        result = test.run()
                 ^^^^^^^^^^
      File "tdd-xunit-example/src/test_case.py", line 16, in run
        method = getattr(self, self.name)
                 ^^^^^^^^^^^^^^^^^^^^^^^^
    AttributeError: 'WasRun' object has no attribute 'testBrokenMethod'

--- src/test_case_test.py
+++ src/test_case_test.py
@@ -8,5 +8,11 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert ("1 run, 0 failed" == result.summary())
 
+    def testFailedResult(self) -> None:
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert ("1 run, 1 failed" == result.summary())
+
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testFailedResult").run()
