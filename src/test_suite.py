6e46b76b76cba59a04e35fe11f5c26a7e53b4799
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-19 17:48:36 +0000

    add class docs to `TestSuite`

--- src/test_suite.py
+++ src/test_suite.py
@@ -5,12 +5,22 @@ if TYPE_CHECKING:
 
 
 class TestSuite:
+    """
+    A TestSuite is a Composite of Tests
+    """
+
     def __init__(self) -> None:
         self.tests = []
 
     def add(self, test: 'TestCase') -> None:
+        """
+        Add a test to the suite
+        """
         self.tests.append(test)
 
     def run(self, result) -> None:
+        """
+        Run all the tests in the suite
+        """
         for test in self.tests:
             test.run(result)
