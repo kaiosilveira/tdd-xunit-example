205e5d5b5ba8465316bc167ec876f55bd5ea75da
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-19 17:46:59 +0000

    add class docs to `TestResult` and address lint warnings

--- src/test_result.py
+++ src/test_result.py
@@ -1,13 +1,27 @@
 class TestResult:
+    """
+    Stores the results of a test run. It keeps track of the number
+    of tests that were run and the number of tests that failed.
+    """
+
     def __init__(self) -> None:
-        self.runCount = 0
-        self.errorCount = 0
+        self.run_count = 0
+        self.error_count = 0
 
     def test_started(self) -> None:
-        self.runCount = self.runCount + 1
+        """"
+        Increments the number of tests that were run.
+        """
+        self.run_count = self.run_count + 1
 
     def test_failed(self) -> None:
-        self.errorCount = self.errorCount + 1
+        """
+        Increments the number of tests that failed.
+        """
+        self.error_count = self.error_count + 1
 
     def summary(self):
-        return "%d run, %d failed" % (self.runCount, self.errorCount)
+        """
+        Returns a string that summarizes the test run.
+        """
+        return f"{self.run_count} run, {self.error_count} failed"
